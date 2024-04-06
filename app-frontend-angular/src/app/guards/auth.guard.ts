abcb4f1c983082297e5182fad1b7db296699a838
Clara Prioux <prioux.clara@gmail.com>
2024-04-06 15:36:00 +0200

    Home component, V1, fixes #11 and fixes #18

--- app-frontend-angular/src/app/guards/auth.guard.ts
+++ app-frontend-angular/src/app/guards/auth.guard.ts
@@ -1,10 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
 
   const auth: AuthService = inject(AuthService);
-  console.log(auth.getLoginStatus());
-  return auth.getLoginStatus();
+  const router: Router = inject(Router);
+
+  const isLoggedIn = auth.getLoginStatus();
+  if (!isLoggedIn) {
+    router.navigate(['/login']);
+  }
+
+  return isLoggedIn;
 };
--- app-frontend-angular/src/app/pages/home/home.component.ts
+++ app-frontend-angular/src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../../services/auth.service';
 import { LoggedInUser } from '../../interfaces/loggedinuser';
 import { Observable } from 'rxjs';
 import { AsyncPipe, CommonModule } from '@angular/common';
+import { RecipesearchService } from '../../services/recipesearch.service';
 
 @Component({
   selector: 'app-home',
@@ -13,8 +14,35 @@ import { AsyncPipe, CommonModule } from '@angular/common';
 })
 export class HomeComponent {
   loggedIn$: Observable<LoggedInUser>;
+  recipes?: any;
+  searchterm = "Popular";
+  randomRecipe: any;
   
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, private recipesearchService: RecipesearchService) {
     this.loggedIn$ = this.auth.loggedIn$;
+    this.searchRecipe();
   }
-}
+
+  searchRecipe() {
+    this.recipesearchService.getRecipes(this.searchterm).subscribe(res=> {
+      console.log(res);
+      let recipeArray: any[];
+      recipeArray = res.hits;
+      console.log(recipeArray);
+  
+      let recipes = recipeArray.map(item => {
+        return {
+          self: item._links.self.href,
+          label: item.recipe.label, 
+          image: item.recipe.image,
+          totalTime: item.recipe.totalTime, 
+          ingredientLines: item.recipe.ingredientLines,
+          url: item.recipe.url,
+        }
+      });
+      console.table(recipes);
+      this.recipes = recipes;
+
+    })
+    }
+  }
